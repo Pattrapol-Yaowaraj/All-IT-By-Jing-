8e30e70d9ccdcb4f604019bfa87108a70556e1c4
Loleo_P <66070148@kmitl.ac.th>
2023-10-10 23:09:04 +0700

    148: Edit Signup Condition
    -Check if Username and Student ID already exist
    -Check Password rules

--- All_IT/accounts/forms.py
+++ All_IT/accounts/forms.py
@@ -13,3 +13,9 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['name', 'sid', 'year', 'major']
+
+    def clean_sid(self):
+        sid = self.cleaned_data['sid']
+        if UserProfile.objects.filter(sid=sid).exists():
+            raise forms.ValidationError("Student ID is already taken.")
+        return sid
--- All_IT/accounts/views.py
+++ All_IT/accounts/views.py
@@ -4,7 +4,7 @@ from .forms import UserRegistrationForm, UserProfileForm
 from django.contrib.auth.models import User
 from .models import UserProfile
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
@@ -12,11 +12,22 @@ def register(request):
         profile_form = UserProfileForm(request.POST)
         
         if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save(commit=False)  # Don't commit yet
+            username = user_form.cleaned_data['username']
+            sid = request.POST.get('sid')  # Correctly get sid from POST data
+            
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username is already taken.')
+                return redirect('registration:register')
+
+            if UserProfile.objects.filter(sid=sid).exists():
+                messages.error(request, 'Student ID is already taken.')
+                return redirect('registration:register')
+
+            user = user_form.save(commit=False)
             user.save()
 
-            profile = profile_form.save(commit=False)  # Don't commit yet
-            profile.user = user  # Associate user with profile
+            profile = profile_form.save(commit=False)
+            profile.user = user
             profile.save()
 
             return redirect('registration:registration_success')
@@ -25,7 +36,6 @@ def register(request):
         profile_form = UserProfileForm()
     
     return render(request, 'account/signup.html', {'user_form': user_form, 'profile_form': profile_form})
-
 def registration_success(request):
     return redirect(reverse('home:Display'))
 
@@ -33,8 +43,4 @@ def validate_credentials(request):
     email = request.POST.get('email')
     password = request.POST.get('password')
 
-    # Perform authentication and credential validation here
-    # ...
-
-    # For now, let's just return a success response
     return JsonResponse({'success': True})
